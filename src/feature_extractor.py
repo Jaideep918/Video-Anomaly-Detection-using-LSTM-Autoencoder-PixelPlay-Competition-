23cce268bb4ac86aaefd03310f0e574345e5dcb2
Jaideep918 <jaideepbanka918@gmail.com>
2026-01-08 14:33:00 +0530

    Add FeatureExtractor class using pretrained ResNet18

---
+++ src/feature_extractor.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+
+class FeatureExtractor(nn.Module):
+    """
+    Uses pretrained ResNet18 as fixed feature extractor.
+    """
+
+    def __init__(self):
+        super().__init__()
+        model = models.resnet18(pretrained=True)
+        self.backbone = nn.Sequential(*list(model.children())[:-1])
+        for p in self.backbone.parameters():
+            p.requires_grad = False
+
+    def forward(self, x):
+        with torch.no_grad():
+            f = self.backbone(x)
+        return f.view(f.size(0), -1)
